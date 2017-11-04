fc9a38b4c04457025f91a691c2f813a5fe6ea23b
[Jesper] <[jp222vq@student.lnu.se]>
2017-11-03 23:38:27 -0400

    Initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XUnit_testing_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
