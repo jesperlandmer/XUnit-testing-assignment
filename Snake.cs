572d8610808c491c62326ccfab91615709c63a6e
[Jesper] <[jp222vq@student.lnu.se]>
2017-11-04 21:49:25 -0400

    GREEN

--- Snake.cs
+++ Snake.cs
@@ -5,26 +5,44 @@ namespace XUnit_testing_assignment
 {
     class Snake
     {
+        const int INITIAL_LENGTH = 5;
+        const int INITIAL_XCOORDINATE = 10;
+        const int INITIAL_YCOORDINATE = 10;
+
         private int _length;
-        private Position _headPosition;
+        public Position HeadPosition  { get; private set; }
         public List<Position> Positions { get; private set; }
 
         public Snake()
         {
-            throw new NotImplementedException("No Constructor Implemented");
+            Length = INITIAL_LENGTH;
+            HeadPosition = new Position(INITIAL_XCOORDINATE, INITIAL_YCOORDINATE);
+            Positions = new List<Position>();
+
+            Positions.Add(HeadPosition);
+            SetBodyPositions();
         }
 
         public void Set(int length, Position headPosition)
         {
-            throw new NotImplementedException("No Set Method Implemented");
+            Length = length;
+            HeadPosition = headPosition;
         }
         public void SetBodyPositions()
         {
-            throw new NotImplementedException("No Set Method for Body Positions Implemented");
+            int x = HeadPosition.XCoordinate - 1;
+            int y = HeadPosition.YCoordinate - 1;
+
+            for (int i = 0; i < Length; i++)
+            {
+                Positions.Add(new Position(x, y));
+                x -= 1;
+                y -= 1;
+            }
         }
         public void GrowSnake()
         {
-            throw new NotImplementedException("No Snake-Growing Method Implemented");
+            Length += 1;
         }
 
         public int Length
@@ -35,19 +53,12 @@ namespace XUnit_testing_assignment
             }
             private set
             {
-                throw new NotImplementedException("No Length Implemented");
-            }
-        }
+                if (value < INITIAL_LENGTH)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value));
+                }
 
-        public Position HeadPosition
-        {
-            get
-            {
-                return _headPosition;
-            }
-            private set
-            {
-                throw new NotImplementedException("No HeadPosition Implemented");
+                _length = value;
             }
         }
     }
